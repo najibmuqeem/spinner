55d4822eab832a9b87f8c05665f8d02bb6880a57
Najib <najib.muqeem@gmail.com>
2020-02-11 17:41:55 +0000

    refactored spinner to remove extraneous setTimeout functions

---
+++ spinner2.js
@@ -0,0 +1,6 @@
+const arr = ["|", "/", "-", "\\", "|", "/", "-", "\\", "|\n"];
+for (let i in arr) {
+  setTimeout(() => {
+    process.stdout.write("\r" + arr[i] + "    ");
+  }, 100 + 200 * i);
+}
